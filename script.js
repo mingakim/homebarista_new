a6c1123daba4ef7f556ea740cf12c3a36fe77208
mingakim <rlaalsdk0302@naver.com>
2025-03-05 18:49:15 +0900

    수정2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 $(function(){
     var headerTop = $('.header-top');
     var header = $('.header');
+    var logo = $('.header .logo');
     var headerTopHeight = headerTop.outerHeight();  // header-top의 높이값
 
     // 스크롤 이벤트
@@ -11,9 +12,13 @@ $(function(){
         if (scrollTop > headerTopHeight) {  // 스크롤이 header-top의 높이를 넘으면
             header.addClass('fixed');  // header를 고정
             headerTop.addClass('hidden');  // header-top 숨기기
+            $('.header > .con, .header .scroll-header').addClass('visible');
+            logo.addClass('hidden');
         } else {
             header.removeClass('fixed');  // header 고정 해제
             headerTop.removeClass('hidden');  // header-top 보이기
+            $('.header > .con, .header .scroll-header').removeClass('visible');
+            logo.removeClass('hidden');
         }
     });
 });
