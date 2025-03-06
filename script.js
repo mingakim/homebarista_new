c43b1ea00d495e1cca783a4c214c140df8d7673f
mingakim <rlaalsdk0302@naver.com>
2025-03-06 18:47:44 +0900

    글자 aos 작업

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ $(function(){
     var headerTop = $('.header-top');
     var header = $('.header');
     var logo = $('.header .logo');
-    var headerTopHeight = headerTop.outerHeight();  // header-top의 높이값
+    var scrollHeader = $('.scroll-header');
+    var minHeight = $('.headerWrap .min-hight');
+    var headerTopHeight = headerTop.outerHeight();  // header의 높이값
 
     // 스크롤 이벤트
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();  // 현재 스크롤 위치
 
-        if (scrollTop > headerTopHeight) {  // 스크롤이 header-top의 높이를 넘으면
-            header.addClass('fixed');  // header를 고정
+        if (scrollTop > headerTopHeight) {  // 스크롤이 header의 높이를 넘으면
             headerTop.addClass('hidden');  // header-top 숨기기
-            $('.header > .con, .header .scroll-header').addClass('visible');
+            header.addClass('hidden');  // header를 고정
             logo.addClass('hidden');
+            scrollHeader.addClass('fixed');
+            minHeight.addClass('visible');
         } else {
-            header.removeClass('fixed');  // header 고정 해제
             headerTop.removeClass('hidden');  // header-top 보이기
-            $('.header > .con, .header .scroll-header').removeClass('visible');
+            header.removeClass('hidden');  // header 고정 해제
             logo.removeClass('hidden');
+            scrollHeader.removeClass('fixed');
+            minHeight.removeClass('visible');
         }
     });
 });
@@ -26,7 +30,11 @@ $(function(){
 
 
 // 헤더-검색버튼 클릭
-$('.header .icons .search-box > button').click(function(){
-    $('.header .icons .search-box').addClass('active');
-    $('.header .icons .search-box > input[type=text]').addClass('active');
+$('.icons .search-box > button').click(function(){
+    $('.icons .search-box').addClass('active');
+    $('.icons .search-box > input[type=text]').addClass('active');
 });
+
+
+
+AOS.init();
