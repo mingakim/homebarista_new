16b5dfa24561aecf6609b4575a6713e0af6d2dc0
mingakim <rlaalsdk0302@naver.com>
2025-03-25 18:47:59 +0900

    main-info 반응형 수정ing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function () {
   var header = $(".header");
   var logo = $(".header .logo");
   var scrollHeader = $("#scroll-header");
-  var minHeight = $(".headerWrap .min-hight");
+  var minHeight = $(".headerWrap .min-height");
   var headerTopHeight = headerTop.outerHeight(); // headertop의 높이값
 
   
@@ -40,7 +40,6 @@ $(".icons .search-box > button").click(function () {
 
 
 
-
 // visual-swiper
 var swiper = new Swiper(".visual-swiper", {
   slidesPerView: 1,
@@ -66,18 +65,21 @@ var swiper = new Swiper(".visual-swiper", {
 // video  크기효과
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".video-box", {
-    width: "100%",  
-    scrollTrigger: {
-        trigger: ".video-box", // 스크롤을 추적할 요소
-        start: "top 85%",  // 시작 위치: 요소가 화면에 들어올 때
-        end: "bottom 100%",    // 끝 위치: video-box의 하단이 화면 bottom에 도달할때
-        scrub: true,          // 스크롤에 비례하여 애니메이션 진행
-        markers: false,       // 디버깅용 마커 표시 (필요시 true로 설정)
-        ease: "none"          // 스크롤에 맞춰 자연스럽게 애니메이션
-    }
+ScrollTrigger.matchMedia({
+  "(min-width: 768px)": function() {
+    gsap.to(".video-box", {
+        width: "100%",  
+        scrollTrigger: {
+            trigger: ".video-box", // 스크롤을 추적할 요소
+            start: "top 85%",  // 시작 위치: 요소가 화면에 들어올 때
+            end: "bottom 100%",    // 끝 위치: video-box의 하단이 화면 bottom에 도달할때
+            scrub: true,          // 스크롤에 비례하여 애니메이션 진행
+            markers: false,       // 디버깅용 마커 표시 (필요시 true로 설정)
+            ease: "none"          // 스크롤에 맞춰 자연스럽게 애니메이션
+        }
+    });
+  }
 });
-
 
 
 
@@ -87,50 +89,64 @@ AOS.init();
 
 
 // monthly-swiper scale모드
-// let $swiperPrev = document.querySelector('.monthly-swiper .swiper-button-prev');
-// var swiper = new Swiper(".monthly-swiper", {
-//   slidesPerView: 3,
-//   // spaceBetween: 135,
-//   centeredSlides: true,
-//   loop: true,
-//   loopedSlides: 2,
-//   // watchOverflow : true,
-//   navigation: {
-//     nextEl: ".monthly-swiper .swiper-button-next",
-//     prevEl: ".monthly-swiper .swiper-button-prev",
-//   },
-//   // on: {
-//   //   activeIndexChange : function() {
-//   //     if (this.realIndex == 0) {
-//   //       $swiperPrev.classList.add('swiper-button-disabled');
-//   //       startNum = false;
-//   //     } else {
-//   //       $swiperPrev.classList.remove('swiper-button-disabled');
-//   //     }
-//   //   }
-//   // }
-// });
-
-
-// monthly-swiper
 var swiper = new Swiper(".monthly-swiper", {
-  effect: "coverflow",
-  grabCursor: true,
+  slidesPerView: 'auto',
+  spaceBetween: 40,
   centeredSlides: true,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 200,
-    modifier: 2,
-    slideShadows: true,
-  },
   loop: true,
   navigation: {
     nextEl: ".monthly-main .swiper-button-next",
     prevEl: ".monthly-main .swiper-button-prev",
-  },  
-  slidesPerView: 3,  
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 'auto',   
+      spaceBetween: 60, 
+    },    
+  },
 });
+
+
+// mb-monthly-swiper
+var swiper = new Swiper(".mb-monthly-swiper", {
+  centeredSlides: true,
+  loop: true, 
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  breakpoints: {
+    440: {
+      slidesPerView: 'auto',   
+      spaceBetween: 25, 
+    },    
+  },
+});
+
+
+// monthly-swiper
+// var swiper = new Swiper(".monthly-swiper", {
+//   effect: "coverflow",
+//   centeredSlides: true,
+//   coverflowEffect: {
+//     rotate: 0,
+//     stretch: 0,
+//     depth: 300,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   loop: true,
+//   navigation: {
+//     nextEl: ".monthly-main .swiper-button-next",
+//     prevEl: ".monthly-main .swiper-button-prev",
+//   },  
+//   slidesPerView: 1.5, 
+//   spaceBetween: 200,
+//   breakpoints: {
+//     1024: {
+//       slidesPerView: 2,
+//       spaceBetween: 0,
+//     },    
+//   }, 
+// });
 
 
 
@@ -153,8 +169,7 @@ var mainSwiper = new Swiper(".best-swiper", {
     
     1150: {
       slidesPerView: 3,
-      spaceBetween: 94, 
-      
+      spaceBetween: 94,       
     },
   },  
 });
